1bc0e9850f9b8e26bec54cedebe0ff3811c4cb80
StarNuik <starnuik@gmail.com>
2017-01-20 21:43:01 +0300

    Reactions are WIP
    Reactions are not working yet.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const
     Eris = require("eris"),
+    rManager = require("./lib/reactionManager"),
+    Log = require("./lib/log"),
     prefix = ">";
 
 var bot = new Eris("MjQzMzU4NDMzNzQ1Njk4ODE4.Cvt1UA.agTtmziRhEO0NBX4A10TQFtgcmA");
 
 bot.on("ready", () => {
-    console.log("Ready!");
+    console.log("I am ready! Nothing crashed, I think.");
     //todo: Actually send a message to the log script that the bot is ready
 });
 
@@ -13,16 +15,66 @@ bot.on("messageCreate", (message) => {
     if (message.content.startsWith(prefix)) {
         //Command route
         if (message.content.indexOf(" ") >= 2) {
-            //I can create an argument kind of thingy in here
+            //Has spaces. Command has to have at least *a* char. '>c'
             output = message.content.substring(1, message.content.indexOf(" "));
+            Log.logAction(output);
             bot.createMessage(message.channel.id, output);
         } else if (!message.content.includes(" ") && message.content[1] !== null) {
-             output = message.content.substring(1, message.content.length);
-             bot.createMessage(message.channel.id, output);
+            //No spaces
+            output = message.content.substring(1, message.content.length);
+            Log.logAction(output);
+            if (output == "die")
+                Die();
+            bot.createMessage(message.channel.id, output);
         }
     } else {
         //Interaction route
+        messageHasName(message.content, (hasName) => {
+            getInteractionNum(message.content, (interactionNum) => {
+                text = rManager.getResponse(interactionNum);
+                bot.createMessage(message.channel.id, text);
+            });
+        })
+        
+        /*This is the pseudo code \/
+        findInter((result)=>{
+            bot.createMessage(channelid, interaction[result])
+        });*/
     }
 });
 
+messageHasName = (messageText, callback) => {
+    rManager.getName((catboiName) => {
+        for (i = 0; i < catboiName.length; i++) {
+            if (messageText.toLowerCase().includes(catboiName[i]))
+                callback();
+        }
+    });
+}
+
+getInteractionNum = (messageText, callback) => {
+    rManager.getLength((err, length) => {
+        for (i = 0; i < length; i++) {
+            Log.getTrigger(i, (triggerArray) => {
+                if (triggerArray !== null) {
+                    for (j = 0; j < triggerArray.length; j++) {
+                        if (messageText.includes(triggerArray[j]))
+                            callback(i);
+                    }
+                }
+            });
+        }
+    });
+}
+
+Die = () => {
+    bot.disconnect();
+    Log.logAction("Catboi shut down");
+    Log.endLog(() => {
+        process.exit();
+    });
+}
+
+Log.startLog();
+rManager.cacheReactions();
 bot.connect();
