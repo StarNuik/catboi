a238600e2fccc04be7c18bee033af2d1c09dbda1
StarNuik <starnuik@gmail.com>
2017-01-24 18:42:36 +0300

    fun: no longer pollutes global space
    I think

--- commands/fun.js
+++ commands/fun.js
@@ -4,19 +4,22 @@ const
     fs = require("fs"),
     pathBall = "./data/8ball.json";
 
-command.add("8ball", (message, args, callback) => {
-    question = message.content.substring(1).replace("8ball", "");
-    sum = 0;
-	for (i = 0; i < question.length; i++)
-		sum += message.content.charCodeAt(i);
-    index = sum % 3;
-    fs.readFile(pathBall, "utf-8", (err, rawData) => {
-        if (err)
-            log.logError("Failed to read the 8ball file", err);
-        data = JSON.parse(rawData);
-        num = Math.floor(Math.random() * data.list[index].item.length);
-        callback(data.list[index].item[num]);
+Fun = function () {
+    command.add("8ball", (message, args, callback) => {
+        question = message.content.substring(1).replace("8ball", "");
+        sum = 0;
+        for (i = 0; i < question.length; i++)
+            sum += message.content.charCodeAt(i);
+        index = sum % 3;
+        fs.readFile(pathBall, "utf-8", (err, rawData) => {
+            if (err)
+                log.logError("Failed to read the 8ball file", err);
+            data = JSON.parse(rawData);
+            num = Math.floor(Math.random() * data.list[index].item.length);
+            callback(data.list[index].item[num]);
+        });
+    }, {
+        argument : true
     });
-}, {
-    argument : true
-});
+}
+module.exports = new Fun();
