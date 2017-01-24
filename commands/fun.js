9dfe716d8f07c43b429d4b8b0e58c7b90f1b1590
StarNuik <starnuik@gmail.com>
2017-01-24 18:30:54 +0300

    fun: 8ball
    Fully working 8ball. Rejoice!

---
+++ commands/fun.js
@@ -0,0 +1,22 @@
+const
+    command = require("../lib/command"),
+    log = require("../lib/log")
+    fs = require("fs"),
+    pathBall = "./data/8ball.json";
+
+command.add("8ball", (message, args, callback) => {
+    question = message.content.substring(1).replace("8ball", "");
+    sum = 0;
+	for (i = 0; i < question.length; i++)
+		sum += message.content.charCodeAt(i);
+    index = sum % 3;
+    fs.readFile(pathBall, "utf-8", (err, rawData) => {
+        if (err)
+            log.logError("Failed to read the 8ball file", err);
+        data = JSON.parse(rawData);
+        num = Math.floor(Math.random() * data.list[index].item.length);
+        callback(data.list[index].item[num]);
+    });
+}, {
+    argument : true
+});
